436758d6c828e4df6fed8e62d0a6ebde51d72be4
Michael Pekkarinen <pekkmj@gmail.com>
2023-02-13 14:25:20 -0500

    integrated edamam API on back end with added files

---
+++ server/apiClient/Edamam.js
@@ -0,0 +1,24 @@
+import got from "got"
+import dotenv from "dotenv"
+dotenv.config();
+
+const edamamApiKey = process.env.EDAMAM_API_KEY
+const edamamApiId = process.env.EDAMAM_APP_ID
+const baseUrl = " https://api.edamam.com/api/recipes/v2"
+const requiredUrl = `${baseUrl}?type=public&app_id=${edamamApiId}&app_key=%20${edamamApiKey}`
+
+class EdamamClient {
+  static async getRecipes(mealType) {
+    try {
+      const url = `${requiredUrl}&mealType=${mealType}`
+      console.log(url)
+      const apiResponse = await got(url)
+      const responseBody = apiResponse.body
+      return responseBody
+    } catch (error) {
+      return { error: error.message };
+    } 
+  }
+}
+
+export default EdamamClient
